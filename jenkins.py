a68b5db30ffd92de5f30240ea013d2ae315ff426
tusharpimple <38343045+tusharpimple@users.noreply.github.com>
2018-06-08 15:28:32 +0530

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,6 +1,9 @@
-import jenkins
-
-server = jenkins.Jenkins('http://13.56.11.237:8080', username='Tushar', password='Rupesh@123')
-user = server.get_whoami()
-version = server.get_version()
-print('Hello %s from Jenkins %s' % (user['Tushar Pimple'], version))
+import requests
+url = "http://13.56.11.237:8080/job/newone/buildwithparameter"
+target = "http://13.56.11.237:8080/"
+headers = {"Content-Type": "application/x-www-form-urlencoded"}
+msg = {
+    'token': '9d1e2b6f0cf80d8040f8c9f65bc7a57d',
+    'target': [ target ],
+}
+r = requests.post(url, headers=headers, data=msg)
